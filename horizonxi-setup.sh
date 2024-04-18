aee2fbb446d8aaffa63d852a4eb24f2c91dd082c
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2024-04-17 21:12:28 -0400

    Checks if Launcher exists then asks to remove it
    Remove Laucher folder and also changed to using $HOME

--- horizonxi-setup.sh
+++ horizonxi-setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 version=1.2.4
 
-rm -ivr ~/Games/HorizonXI/Launcher
-mkdir -p ~/Games/HorizonXI/{Launcher,Prefix}
-cd ~/Games/HorizonXI/Launcher
+if [ -d "$HOME/Games/HorizonXI/Launcher" ]; then
+echo "Launcher folder already exists, running remove command on it. Press y to remove it when asked."
+rm -Ivr $HOME/Games/HorizonXI/Launcher
+fi
+mkdir -p $HOME/Games/HorizonXI/{Launcher,Prefix}
+cd $HOME/Games/HorizonXI/Launcher
 wget -c https://github.com/HorizonFFXI/HorizonXI-Launcher-Binaries/releases/download/v$version/HorizonXI-Launcher-$version.Setup.exe && \
 7z x -i"!*.nupkg" HorizonXI-Launcher-$version.Setup.exe && \
 7z x -i"!lib/net45/*" HorizonXI_Launcher-$version-full.nupkg && \
