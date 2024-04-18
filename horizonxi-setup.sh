ee5695a892116cad124b74997c51ec3d241b11db
Daniel Melzak <43735175+TeamLinux01@users.noreply.github.com>
2024-04-17 20:46:20 -0400

    Update horizonxi-setup.sh
    Removed no-clobber switch from wget

--- horizonxi-setup.sh
+++ horizonxi-setup.sh
@@ -3,7 +3,7 @@ version=1.2.4
 
 mkdir -p ~/Games/HorizonXI/{Launcher,Prefix}
 cd ~/Games/HorizonXI/Launcher
-wget -nc -c https://github.com/HorizonFFXI/HorizonXI-Launcher-Binaries/releases/download/v$version/HorizonXI-Launcher-$version.Setup.exe && \
+wget -c https://github.com/HorizonFFXI/HorizonXI-Launcher-Binaries/releases/download/v$version/HorizonXI-Launcher-$version.Setup.exe && \
 7z x -i"!*.nupkg" HorizonXI-Launcher-$version.Setup.exe && \
 7z x -i"!lib/net45/*" HorizonXI_Launcher-$version-full.nupkg && \
 mv lib/net45/* . && \
