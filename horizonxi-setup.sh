aeb9b8963a6487dbc23b3f456f23976dbe7e8bcc
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2024-04-20 12:57:41 -0400

    Extract all files and don't remove any

--- horizonxi-setup.sh
+++ horizonxi-setup.sh
@@ -5,15 +5,12 @@ if [ -d "$HOME/Games/HorizonXI/Launcher" ]; then
 echo "Launcher folder already exists, running remove command on it. Press y to remove it when asked."
 rm -Ivr $HOME/Games/HorizonXI/Launcher
 fi
-mkdir -p $HOME/Games/HorizonXI/{Launcher,Prefix}
-cd $HOME/Games/HorizonXI/Launcher
+mkdir -p $HOME/Games/HorizonXI/{Launcher/download,Prefix}
+cd $HOME/Games/HorizonXI/Launcher/download
 wget -c https://github.com/HorizonFFXI/HorizonXI-Launcher-Binaries/releases/download/v$version/HorizonXI-Launcher-$version.Setup.exe && \
-7z x -i"!*.nupkg" HorizonXI-Launcher-$version.Setup.exe && \
-7z x -i"!lib/net45/*" HorizonXI_Launcher-$version-full.nupkg && \
-mv lib/net45/* . && \
-rmdir lib/net45 && \
-rmdir lib && \
-rm -v HorizonXI-Launcher-$version.Setup.exe HorizonXI_Launcher-$version-full.nupkg
+7z x HorizonXI-Launcher-$version.Setup.exe && \
+7z x HorizonXI_Launcher-$version-full.nupkg && \
+mv lib/net45/* $HOME/Games/HorizonXI/Launcher/
 if [ ! -d "$HOME/Games/HorizonXI/Prefix/pfx" ]; then
 mkdir -p $HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher
 cd $HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher
