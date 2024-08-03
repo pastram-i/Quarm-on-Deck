f25631b38b2c174309ed01db88a8604f0ae3358f
Daniel Melzak <43735175+TeamLinux01@users.noreply.github.com>
2024-08-03 12:49:57 -0400

    Only download if json files don't exist

--- horizonxi-setup.sh
+++ horizonxi-setup.sh
@@ -2,8 +2,8 @@
 version=1.3.0
 
 if [ -d "$HOME/Games/HorizonXI/Launcher" ]; then
-echo "Launcher folder already exists, running remove command on it. Press y to remove it when asked."
-rm -Ivr "$HOME"/Games/HorizonXI/Launcher
+  echo "Launcher folder already exists, running remove command on it. Press y to remove it when asked."
+  rm -Ivr "$HOME"/Games/HorizonXI/Launcher
 fi
 mkdir -p "$HOME"/Games/HorizonXI/{Launcher/download,Prefix}
 cd "$HOME"/Games/HorizonXI/Launcher/download || exit 1
@@ -12,10 +12,14 @@ wget -c https://github.com/HorizonFFXI/HorizonXI-Launcher-Binaries/releases/down
 7z x HorizonXI_Launcher-$version-full.nupkg && \
 mv lib/net45/* "$HOME"/Games/HorizonXI/Launcher/
 if [ ! -d "$HOME/Games/HorizonXI/Prefix/pfx" ]; then
-mkdir -p "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher
-cd "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher || exit 1
-wget -c https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json
-wget -c https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json
+  mkdir -p "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher
+  cd "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher || exit 1
+  if [ ! -f "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json ]; then
+    wget https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json
+  fi
+  if [ ! -f "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json ]; then
+    wget -c https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json
+  fi
 fi
 echo "Copying install files completed. It is safe to close the terminal."
 exit 0
