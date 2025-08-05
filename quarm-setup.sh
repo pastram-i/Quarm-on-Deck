9125323640c528ec7906a26db644ace68d9a1a81
pastram-i <73852781+pastram-i@users.noreply.github.com>
2025-08-04 21:45:55 -0500

    Update quarm-setup.sh

--- quarm-setup.sh
+++ quarm-setup.sh
@@ -1,4 +1,4 @@
-u
+#!/bin/bash
 version=0.1.0
 
 # if [ -d "$HOME/Games/Quarm/Launcher" ]; then
@@ -6,23 +6,31 @@ version=0.1.0
 #   rm -Ivr "$HOME/Games/Quarm/Launcher"
 # fi
 
-mkdir -p "$HOME/Games/Quarm/"{Downloads,Launcher/download,Prefix}
+# 1 - Download TAKP
+mkdir -p "$HOME/Games/Quarm/"
 pushd "$HOME/Games/Quarm" || exit 1
 curl -L "https://drive.usercontent.google.com/download?id=1qoBktDeJMJKPBr-EZxub1vspJhz11i1y&export=download&confirm=t" -o "$HOME/Downloads/quarm.zip" && \
 7z x "$HOME/Downloads/quarm.zip" -o "$HOME/Games/Quarm"
 popd || exit 1
-if [ ! -d "$HOME/Games/HorizonXI/Prefix/pfx" ]; then
-  mkdir -p "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/Program Files (x86)/HorizonXI"
-  ln -s "$HOME/Games/HorizonXI/Downloads" "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/Program Files (x86)/HorizonXI/Downloads"
-  mkdir -p "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher"
-  pushd "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher" || exit 1
-  if [ ! -f "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json" ]; then
-    wget https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json
-  fi
-  if [ ! -f "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json ]; then
-    wget https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json
-  fi
-  popd || exit 1
-fi
-echo "Copying install files completed. It is safe to close the terminal."
-exit 0
+
+# 2 - Download Quarm Patcher
+# 3 - Download Zeal
+# 4 - Download Zeal compatible UI
+# 5 - Download dgvoodoo config
+
+
+# if [ ! -d "$HOME/Games/HorizonXI/Prefix/pfx" ]; then
+#   mkdir -p "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/Program Files (x86)/HorizonXI"
+#   ln -s "$HOME/Games/HorizonXI/Downloads" "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/Program Files (x86)/HorizonXI/Downloads"
+#   mkdir -p "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher"
+#   pushd "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher" || exit 1
+#   if [ ! -f "$HOME/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json" ]; then
+#     wget https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/config.json
+#   fi
+#   if [ ! -f "$HOME"/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json ]; then
+#     wget https://raw.githubusercontent.com/TeamLinux01/HorizonXI-on-Deck/main/Games/HorizonXI/Prefix/pfx/drive_c/users/steamuser/AppData/Roaming/HorizonXI-Launcher/storage.json
+#   fi
+#   popd || exit 1
+# fi
+# echo "Copying install files completed. It is safe to close the terminal."
+# exit 0
